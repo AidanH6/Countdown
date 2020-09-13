174c449cb1cbabd7e439d5493fa687e8aab9e344
Aidan Hopkins <40990017+AidanH6@users.noreply.github.com>
2020-09-13 17:17:16 +0100

    implimenting modal changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,24 @@
 /* Javascript for countdown website */
 /* Copyright Aidan Hopkins 2020 */
 
-const vueMessage = 'Hello';
+const toShow = "Vue is working as intended";
 
-var app = new Vue({
-    el: '#app',
+Vue.component( "modal", {
+    template: '#modal-template'
+})
+
+var modal = new Vue({
+    el: '#modal',
     data: {
-        message: vueMessage
+        showModal: true
     }
 });
+
+var text = new Vue({
+    el: '#app',
+    data: {
+        message: ''
+    }
+});
+
+text.message = toShow;
