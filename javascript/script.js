cc5263a6051a4cd2779991d7d4b52f53b85cd040
Aidan Hopkins <40990017+AidanH6@users.noreply.github.com>
2020-09-13 15:39:25 +0100

    Add JS file and html changes

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+/* Javascript for countdown website */
+/* Copyright Aidan Hopkins 2020 */
+
+const message = 'Hello';
+
+new Vue({
+    el: '#app',
+    data: {
+        message: message
+    }
+});
